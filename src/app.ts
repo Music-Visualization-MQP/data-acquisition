2a8610bf30564157f68514fe4a0cc59afd45c65c
ibixler <108148378+ibixler@users.noreply.github.com>
2024-06-06 14:31:00 -0400

    confirmed spotify api js wrapper worked as expected

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
-import express from "express";
 import dotenv from "dotenv";
+import {Client, Player} from "spotify-api.js"
 dotenv.config();
 
 console.log(process.env.ANON);
 // Create a single supabase client for interacting with your database
-const supabase : SupabaseClient = createClient(
+const supabase: SupabaseClient = createClient(
   process.env.SB_URL as string,
   process.env.SERVICE as string,
   {
@@ -15,11 +15,30 @@ const supabase : SupabaseClient = createClient(
       detectSessionInUrl: false,
     },
   }
-) as SupabaseClient;
+)
 
 async function func() {
-  const { data: credsData, error: grabError } = await supabase.from('spotify_credentials').select('*')
-  return { credsData, grabError };
+  const client = await Client.create({
+    refreshToken: true, 
+    token: {
+      clientID: process.env.SP_CID as string,
+      clientSecret: process.env.SP_SECRET as string,
+      refreshToken: '',
+  },
+    onRefresh() {
+      console.log(`Token has been refreshed. New token: ${client.token}!`);
+    },
+  });
+  const { data: credsData, error: grabError } = await supabase
+    .from("spotify_credentials")
+    .select("*");
+  
+  const player = new Player(client);
+  const currentPlayback = await player.getCurrentPlayback();
+  console.log(currentPlayback);
+  return { credsData, grabError, client };
+
+
 }
 
 func().then((ret) => console.log(ret));
