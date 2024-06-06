76909bb82e50941ed24ff671cbcd82332facb30f
ibixler <108148378+ibixler@users.noreply.github.com>
2024-06-06 10:59:44 -0400

    Basic script for testing
    TODO: rename the repo to something more helpful
    
    The eventual purpose of this application will be to handle the gathering of userdata, and then storing in the database.
    
    Right now it just serves to make sure the service key functions. The service key just bypasses row level security so it can access all of the data. The idea is that this part of the application will not have any exposed endpoints. Which i hope will help with some semblance of security.

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:lts-alpine3.20
+WORKDIR /app
+COPY yarn.lock package.json ./
+
+RUN yarn install
+
+COPY . .
+
+
+EXPOSE 3000
+
+CMD ["yarn", "dev", "--host", "0.0.0.0"]
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
-import { createClient } from '@supabase/supabase-js'
-import express from 'express';
-import dotenv from 'dotenv';
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-require('dotenv').config()
-
-const env = process.env.NODE_ENV;
-
-
-
+console.log(process.env.ANON);
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.SB_URL as string, process.env.ANON as string)
+const supabase : SupabaseClient = createClient(
+  process.env.SB_URL as string,
+  process.env.SERVICE as string,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+      detectSessionInUrl: false,
+    },
+  }
+) as SupabaseClient;
+
+async function func() {
+  const { data: credsData, error: grabError } = await supabase.from('spotify_credentials').select('*')
+  return { credsData, grabError };
+}
+
+func().then((ret) => console.log(ret));
